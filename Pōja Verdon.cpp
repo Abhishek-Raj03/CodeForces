961b48234b4a7f61a6ef541cbd0217e6ff85dfaf
Abhishek-Raj03 <102483394+Abhishek-Raj03@users.noreply.github.com>
2022-04-05 00:31:10 +0530

    Create Pōja Verdon.cpp

---
+++ Pōja Verdon.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i=0;i<n;i++){
+
+        int t;
+        cin>>t;
+        sum+=t;
+    }
+    cout<<sum;
+}
